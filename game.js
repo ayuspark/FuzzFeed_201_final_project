f74b514f8ff369ac4a064d2bfc22b17ce034e6f4
ayuspark <lola.luyaowang@gmail.com>
2017-07-31 16:01:56 -0700

    shaking and burring img

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 'use strict';
+var coding = '20px'; //need to get rid of
+var drink = '0.12';
 //CREATE IMG
 function Images(name) {
   this.name = name;
@@ -15,7 +17,9 @@ for(var i = 0; i < Images.allNames.length; i++){
   new Images(Images.allNames[i]);
 }
 
+//ASSIGN ANSWERS TO each img object
 Images.all[0].answer = [{y:'1 bag'},{n1: '2bags'},{n2: '3bags'}];
+Images.all[1].answer = [{n1:'1 bag'},{y: '2bags'},{n2: '3bags'}];
 
 Images.previousImg = [];
 
@@ -30,19 +34,22 @@ function loadImg(){
     document.getElementById('img_box').appendChild(imgEl);
     Images.previousImg.push(randomIndex);
 
-    for(var i = 0; i < Images.all[0].answer.length; i++){
+//!!!!!!!!!!!!!the following line needs a for loop for ALL imgs
+    for(var i = 0; i < Images.all[1].answer.length; i++){
       var btnEl = document.createElement('button');
-      btnEl.textContent = Images.all[0].answer[i];
-      btnEl.setAttribute('id', Object.keys(Images.all[0].answer[i]));
+      btnEl.textContent = Object.values(Images.all[1].answer[i]);
+      btnEl.setAttribute('id', Object.keys(Images.all[1].answer[i]));
       document.getElementById('answers').appendChild(btnEl);
     }
 
   } else {
     loadImg();
   }
+  imageRender('0.12', '10px');//test, need to delete
 }
 //PAGE LOAD IMG
 loadImg();
+imageRender(drink, coding);
 
 function answersHandler(e) {
   console.log(e.target.id);
@@ -58,3 +65,14 @@ function answersHandler(e) {
 
 //EVENT listener
 document.getElementById('answers').addEventListener('click', answersHandler);
+
+//IMG ALTERING FUNCTION
+//test:
+
+function imageRender(drink, coding){
+  var shakeLevel = drink + 's';
+  var blurLevel = 'blur(' + coding + ')';
+  document.getElementById('img_box').children[0].style.animationName = 'shake ';
+  document.getElementById('img_box').children[0].style.animationDuration = shakeLevel ;
+  document.getElementById('img_box').children[0].style.WebkitFilter = blurLevel;
+}
