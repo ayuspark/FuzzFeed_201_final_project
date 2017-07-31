7586147e5cf413887b59f1b7770c5dcec54cfd15
Adam Walls <awalls1@gmail.com>
2017-07-31 16:03:04 -0700

    restructured page layout. Still need to add localStorage functions in JS

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// 'use strict';
+//
+// localStorage.setItem('lsClicks', JSON.stringify(Pictures.all));
+// console.log('localStorage inialized or appended');
 
+
+// ***** Recall localStorage *****
+//Pictures.all = JSON.parse(localStorage.lsClicks);
