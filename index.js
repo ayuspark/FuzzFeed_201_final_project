c866481f16b2228cb65158739d0ee7ccc075ebb0
Adam Walls <awalls1@gmail.com>
2017-08-01 14:37:16 -0700

    minor code clean-up to html and js

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ function selectHandler(event){
   var coding = event.target.codeSelect.value;
   localStorage.setItem('drink', JSON.stringify(drink));
   localStorage.setItem('coding', JSON.stringify(coding));
-  location.href = 'game.html'
+
+  // if(nothing is selected){
+  //
+  // } else {
+   location.href = 'game.html' // Will jump to game page
+  // }
 } // selectHandler END
 
 // Listen for submit button clickage
 document.getElementById('selectInterface').addEventListener('submit', selectHandler);
+
+
+// ADD IF/ELSE FOR WHEN NOTHING IS SELECTED
