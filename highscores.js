916d86e95d93587b51ce109d3a91d90771cdc7da
philipwerner <philip.r.werner@gmail.com>
2017-08-01 15:07:42 -0700

    minor styling changes, got highscore list to populate

--- highscores.js
+++ highscores.js
@@ -1,36 +1,33 @@
 'use strict'
 
-var scoreTable = getElementById('scoresTable');
-var newScores = [];
 
-var HighScores = function(name, score){
+var theScores = [];
+
+function HighScore (name, score){
   this.name = name;
   this.score = score;
+  theScores.push(this);
 };
 
-new HighScores = ('Phil', 500);
-new HighScores = ('Lola', 600);
-new HighScores = ('Brandon', 700);
-new HighScores = ('Adam', 800);
+new HighScore ('Adam', 800);
+new HighScore ('Phil', 500);
+new HighScore ('Lola', 600);
+new HighScore ('Brandon', 700);
 
-// function sortScores () {
-//   newScores = newScores.sort();
-// }
-
-HighScores.prototype.render = function() {
+function allScores(){
   var olEl = document.getElementById('highscores');
 
-  for(var i = 0; i < HighScores.all.length; i++){
+  for(var i = 0; i < theScores.length; i++){
     var liEl = document.createElement('li');
-    liEl.textContent = HighScores.all[i].name + '  ' + HighScores.all[i].score;
+    liEl.textContent = theScores[i].name + ' scored ' + theScores[i].score;
     olEl.appendChild(liEl);
   }
-}
+};
+
 
 // if(local storage){
 //   run function to add new scores to high score table
 // } else {
 //   load default high scores table
 // }
-
-HighScores.render();
+allScores();
