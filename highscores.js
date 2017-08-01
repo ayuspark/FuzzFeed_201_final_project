e62b16814c54b6230f94f87fe4b2c6769ba9c2a4
philipwerner <philip.r.werner@gmail.com>
2017-08-01 16:08:21 -0700

    changed to a table format, still need to populate user and score into table

--- highscores.js
+++ highscores.js
@@ -2,6 +2,7 @@
 
 
 var theScores = [];
+var scoresTable = document.getElementById('highscores');
 
 function HighScore (name, score){
   this.name = name;
@@ -14,15 +15,41 @@ new HighScore ('Phil', 500);
 new HighScore ('Lola', 600);
 new HighScore ('Brandon', 700);
 
+// function sortScores(){
+//   orderedScores =
+// };
+
 function allScores(){
-  var olEl = document.getElementById('highscores');
 
   for(var i = 0; i < theScores.length; i++){
-    var liEl = document.createElement('li');
-    liEl.textContent = theScores[i].name + ' scored ' + theScores[i].score;
-    olEl.appendChild(liEl);
+    var trEl = document.createElement('tr');
+    var tdEl = document.createElement('td');
+    tdEl.textContent = theScores[i].name;
+    trEl.appendChild(tdEl);
   }
+  scoresTable.appendChild(trEl);
+  for(var n = 0; n < theScores.length; n++){
+    trEl = document.createElement('tr');
+    tdEl = document.createElement('td');
+    tdEl.textContent = theScores[n].score;
+    trEl.appendChild(tdEl);
+  }
+  scoresTable.appendChild(trEl);
 };
+
+function tableHeader(){
+  var trEl = document.createElement('tr');
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = 'Score';
+  trEl.appendChild(thEl);
+  scoresTable.appendChild(trEl);
+  allScores();
+}
 
 
 // if(local storage){
@@ -30,4 +57,5 @@ function allScores(){
 // } else {
 //   load default high scores table
 // }
-allScores();
+tableHeader();
+// allScores();
