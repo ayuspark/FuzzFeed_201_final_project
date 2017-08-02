e50e5142f3c66f07427f44a78e6c4a0669bfe464
Adam Walls <awalls1@gmail.com>
2017-08-02 09:30:45 -0700

    added if/else for main page selections

--- index.js
+++ index.js
@@ -3,20 +3,16 @@
 function selectHandler(event){
   event.preventDefault();
 
-  var drink = event.target.drinkSelect.value;
-  var coding = event.target.codeSelect.value;
-  localStorage.setItem('drink', JSON.stringify(drink));
-  localStorage.setItem('coding', JSON.stringify(coding));
-
-  // if(nothing is selected){
-  //
-  // } else {
-   location.href = 'game.html' // Will jump to game page
-  // }
+  if(!event.target.drinkSelect.value || !event.target.codeSelect.value){
+    alert('Please use the drop down to select your drinks and coding time.');
+  } else {
+    var drink = event.target.drinkSelect.value;
+    var coding = event.target.codeSelect.value;
+    localStorage.setItem('drink', JSON.stringify(drink));
+    localStorage.setItem('coding', JSON.stringify(coding));
+    location.href = 'game.html' // Will jump to game page
+  }
 } // selectHandler END
 
 // Listen for submit button clickage
 document.getElementById('selectInterface').addEventListener('submit', selectHandler);
-
-
-// ADD IF/ELSE FOR WHEN NOTHING IS SELECTED
