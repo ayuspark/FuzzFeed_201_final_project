89c227a840b6edec8b94f6ad09beeb6a9f7406d0
philipwerner <philip.r.werner@gmail.com>
2017-08-03 10:51:09 -0700

    added new nav bar, fixed functionality on high scores

--- highscores.js
+++ highscores.js
@@ -62,7 +62,23 @@ function tableScores(){
   }
 };
 
-// localStorage.setItem('theScores', JSON.stringify(theScores));
+// menu javascript
+function openMenuHandler(e){
+  if(e.target.className === 'mini_menu_icon'){
+    document.getElementsByClassName('menu')[0].style.display = 'block';
+    document.getElementsByClassName('mini_menu_icon')[0].style.display = 'none';
+  }
+}
+
+function closeMenuHandler(e) {
+  if(e.target.className === 'close'){
+    document.getElementsByClassName('menu')[0].style.display = 'none';
+    document.getElementsByClassName('mini_menu_icon')[0].style.display = 'block';
+  }
+}
+
+document.getElementById('nav_bar').addEventListener('click', openMenuHandler);
+document.getElementById('nav_bar').addEventListener('click', closeMenuHandler);
 
 if(!localStorage){
   // theScores = JSON.parse(localStorage.getItem('theScores'));
